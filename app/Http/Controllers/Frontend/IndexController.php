6408428296b9b346bd6b0f1e6c96ec7644678284
jasmin tamang <jasmin.softtechpark@gmail.com>
2022-01-25 18:10:41 +0545

    sdvbg

--- app/Http/Controllers/Frontend/IndexController.php
+++ app/Http/Controllers/Frontend/IndexController.php
@@ -9,40 +9,18 @@ use App\Models\SliderImage;
 use App\Models\Image;
 use App\Models\ImageRoom;
 use App\Models\Room;
+use App\Models\Service;
 use Illuminate\Support\Facades\DB;
 
 class IndexController extends Controller
 {
     public function index(){
-        $sliders = Slider::latest()->get();
-        $slidersImages = [];
-        foreach($sliders as $slides){
-            $imageDetail = DB::table('slider_images as SI')
-                            ->join('images as I', 'I.id', '=', 'SI.image_id')
-                            ->select('I.*')
-                            ->where('SI.slider_id', $slides->id)
-                            ->get();
-            $slidersImages[$slides->id] = $imageDetail; 
-        }
-        $services = DB::table('services as S')
-        ->join('images as I', 'I.id', '=', 'S.media_id')
-        ->select('S.*', 'I.file_path')->skip(0)->take(3)
-        ->get();
+        $sliders = Slider::with('images')->latest()->get();
 
-        $filePaths = [];
-        foreach($services as $s){
-            $filePaths[$s->id] = $s->file_path;
-        }
+        $services = Service::with('image')->latest()->get();
 
-        $rooms = Room::latest()->skip(0)->take(5)->get();
-        $roomImage = [];
-        foreach($rooms as $room){
-            $roomImage[$room->id] = DB::table('image_rooms as IR')
-                                    ->join('images as I', 'I.id', '=', 'IR.image_id')
-                                    ->select('IR.*', 'I.file_path')
-                                    ->where('IR.room_id', $room->id)
-                                    ->skip(0)->take(1)->get();
-        }
-        return view('frontend.index',compact('sliders', 'slidersImages', 'services', 'filePaths','rooms','roomImage'));
+        $rooms = Room::with('images')->latest()->take(5)->get();
+
+        return view('frontend.index',compact('sliders','services','rooms'));
     }
 }
