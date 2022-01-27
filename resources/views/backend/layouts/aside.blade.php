f04052dfbb2a961f0f570c06fb411a4482eef27d
jasmin tamang <jasmin.softtechpark@gmail.com>
2022-01-27 20:52:05 +0545

    added menu acive in backend

--- resources/views/backend/layouts/aside.blade.php
+++ resources/views/backend/layouts/aside.blade.php
@@ -12,8 +12,15 @@
       <!-- Sidebar Menu -->
       <nav class="mt-2">
         <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-          
-          <li class="nav-item">
+          <li class="nav-item  {{request()->is('backend/dashboard') ? 'menu-open' : '' }}">
+            <a href="{{route('dashboard')}}" class="nav-link {{request()->is('admin/dashboard') ? 'active' : '' }}">
+              <i class="nav-icon fas fa-tachometer-alt"></i>
+              <p>
+                Dashboard
+              </p>
+            </a>
+          </li>
+          <li class="nav-item {{request()->is('backend/room*') ? 'menu-open' : '' }}">
             <a href="#" class="nav-link">
               <i class="nav-icon fas fa-hotel"></i>
               <p>
@@ -21,22 +28,22 @@
                 <i class="right fas fa-angle-left"></i>
               </p>
             </a>
-            <ul class="nav nav-treeview">
+            <ul class="nav nav-treeview ">
               <li class="nav-item">
-                <a href="{{route('room.create')}}" class="nav-link">
+                <a href="{{route('room.create')}}" class="nav-link  {{request()->is('backend/room/create') ? 'active' : '' }}">
                   <i class="fas fa-plus nav-icon"></i>
                   <p>New Hotel Rooms</p>
                 </a>
               </li>
               <li class="nav-item">
-                <a href="{{route('room.index')}}" class="nav-link">
+                <a href="{{route('room.index')}}" class="nav-link {{request()->is('backend/room') ? 'active' : '' }}">
                   <i class="fas fa-hotel nav-icon"></i>
                   <p>Hotel Rooms</p>
                 </a>
               </li>
             </ul>
           </li>
-          <li class="nav-item">
+          <li class="nav-item {{request()->is('backend/event*') ? 'menu-open' : '' }}">
             <a href="#" class="nav-link">
               <i class="nav-icon fas fa-calendar"></i>
               <p>
@@ -46,20 +53,20 @@
             </a>
             <ul class="nav nav-treeview">
               <li class="nav-item">
-                <a href="{{route('event.create')}}" class="nav-link">
+                <a href="{{route('event.create')}}" class="nav-link {{request()->is('backend/event/create') ? 'active' : '' }}">
                   <i class="fas fa-plus nav-icon"></i>
                   <p>Add New Events</p>
                 </a>
               </li>
               <li class="nav-item">
-                <a href="{{route('event.index')}}" class="nav-link">
+                <a href="{{route('event.index')}}" class="nav-link {{request()->is('backend/event') ? 'active' : '' }}">
                   <i class="far fa-calendar nav-icon"></i>
                   <p>Event Details</p>
                 </a>
               </li>
             </ul>
           </li>
-          <li class="nav-item">
+          <li class="nav-item {{request()->is('backend/service*') ? 'menu-open' : '' }}">
             <a href="#" class="nav-link">
               <i class="fa fa-rocket nav-icon"></i>
               <p>
@@ -69,20 +76,20 @@
             </a>
             <ul class="nav nav-treeview">
               <li class="nav-item">
-                <a href="{{route('service.create')}}" class="nav-link">
+                <a href="{{route('service.create')}}" class="nav-link {{request()->is('backend/service*') ? 'active' : '' }}">
                   <i class="fas fa-plus nav-icon"></i>
                   <p>Add New Service</p>
                 </a>
               </li>
               <li class="nav-item">
-                <a href="{{route('service.index')}}" class="nav-link">
+                <a href="{{route('service.index')}}" class="nav-link {{request()->is('backend/service') ? 'active' : '' }}">
                   <i class="fa fa-rocket nav-icon"></i>
                   <p>Service Details</p>
                 </a>
               </li>
             </ul>
           </li>
-          <li class="nav-item">
+          <li class="nav-item {{request()->is('backend/slider*') ? 'menu-open' : '' }}">
             <a href="#" class="nav-link">
               <i class="nav-icon fas fa-sliders-h"></i>
               <p>
@@ -92,20 +99,20 @@
             </a>
             <ul class="nav nav-treeview">
               <li class="nav-item">
-                <a href="{{route('slider.create')}}" class="nav-link">
+                <a href="{{route('slider.create')}}" class="nav-link {{request()->is('backend/slider/create') ? 'active' : '' }}">
                   <i class="fas fa-plus nav-icon"></i>
                   <p>Add New Slider</p>
                 </a>
               </li>
               <li class="nav-item">
-                <a href="{{route('slider.index')}}" class="nav-link">
+                <a href="{{route('slider.index')}}" class="nav-link {{request()->is('backend/slider') ? 'active' : '' }}">
                   <i class="fas fa-sliders-h nav-icon"></i>
                   <p>Slider Details</p>
                 </a>
               </li>
             </ul>
           </li>
-          <li class="nav-item">
+          <li class="nav-item {{request()->is('backend/contactus*') ? 'menu-open' : '' }}">
             <a href="#" class="nav-link">
               <i class="nav-icon fa fa-address-book"></i>
               <p>
@@ -115,14 +122,14 @@
             </a>
             <ul class="nav nav-treeview">
               <li class="nav-item">
-                <a href="{{route('contactus.index')}}" class="nav-link">
+                <a href="{{route('contactus.index')}}" class="nav-link {{request()->is('backend/contactus*') ? 'active' : '' }}">
                   <i class="fa fa-address-book nav-icon"></i>
                   <p>Contact Details</p>
                 </a>
               </li>
             </ul>
           </li>
-          <li class="nav-item">
+          <li class="nav-item {{request()->is('backend/bookings*') ? 'menu-open' : '' }}">
             <a href="#" class="nav-link">
               <i class="nav-icon fas fa-bed"></i>
               <p>
@@ -132,19 +139,19 @@
             </a>
             <ul class="nav nav-treeview">
               <li class="nav-item">
-                <a href="{{route('booked.room')}}" class="nav-link">
+                <a href="{{route('booked.room')}}" class="nav-link {{request()->is('backend/bookings/booked-room') ? 'active' : '' }}">
                   <i class="fas fa-bed nav-icon"></i>
                   <p>Booked Room</p>
                 </a>
               </li>
               <li class="nav-item">
-                <a href="{{route('bookingrequest.index')}}" class="nav-link">
+                <a href="{{route('bookingrequest.index')}}" class="nav-link {{request()->is('backend/bookings') ? 'active' : '' }}">
                   <i class="fas fa-bed nav-icon"></i>
                   <p>Booking Request</p>
                 </a>
               </li>
             </ul>
-              <li class="nav-item">
+              <li class="nav-item {{request()->is('backend/setting') ? 'menu-open' : '' }}">
               <a href="{{route('setting.index')}}" class="nav-link">
                 <i class="nav-icon fas fa-cog"></i>
                 <p>
